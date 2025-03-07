2e5e82cbc9c7ea356fc7d488604237ef314d91f8
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2025-03-07 15:12:30 -0500

    added schemas to server.js

--- our_app/server/server.js
+++ our_app/server/server.js
@@ -7,4 +7,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const BankAccounts = require("./models/bankaccounts.js");
+const Transactions = require("./models/transactions.js");
+const Users = require("./models/users.js");
+const Categories = require("./models/categories.js");
+
+
+const mongoURI = "mongodb://localhost:27017/HackKnight2025";
+mongoose.connect(mongoURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () =>{
+    console.log("Connected to Mongo");
+})
 
