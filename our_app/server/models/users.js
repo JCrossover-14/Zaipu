943f904762d1ce435426909e220271fa3f2f392b
Andrew Fung <andrew@Andrews-MacBook-Pro.local>
2025-03-07 18:40:12 -0500

    a

--- our_app/server/models/users.js
+++ our_app/server/models/users.js
@@ -6,8 +6,8 @@ var userSchema = new Schema({
     username:{type:String, required:true},
     email:{type:String},
     passwordHash:{type:String, required:true},
-    accountIds:{type: List[String], default: []},
-    categories: {type: List[String], default: []}, 
+    accountIds:{type: [String], default: []},
+    categories: {type: [String], default: []}, 
 });
 
 userSchema.methods.isValidPassword = async function(password) {
