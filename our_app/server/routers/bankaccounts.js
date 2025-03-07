c093ea38f23026fdae84e3a1bbdc79c7a3de822c
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2025-03-07 17:52:36 -0500

    hi

--- our_app/server/routers/bankaccounts.js
+++ our_app/server/routers/bankaccounts.js
@@ -4,19 +4,8 @@ let router = express.Router();
 
 const app = express();
 
-let Category = require("../models/categories");
 
 
-router.post("/category", async (req, res) =>{
-    console.log(req.body);
-    try{
-        const category = new Category(req.body);
-        await category.save();
-    } catch(error){
-        console.log(error)
-        res.status(500).send("Internal server error");
-    }
-    
-})
+
 
 
--- our_app/server/routers/categories.js
+++ our_app/server/routers/categories.js
@@ -1,25 +1,20 @@
+let mongoose = require('mongoose');
+
 let express = require('express');
 let router = express.Router();
-let Categories = require("../models/categories");
-const mongoose = require("mongoose"); 
 
-// create a new list of cateogries for new user 
-router.post("/create/:user_id", async (req, res) =>{
-    user_id = req.params.id = user_id; 
-    Categories.create({user_id: mongoose.Types.ObjectId(user_id), cateogires: []})
-}) 
 
-// find categories based on user_id 
-router.get("/getByUserId/:user_id", async (req, res) => {
+let Category = require("../models/categories");
+
+
+router.post("/category", async (req, res) =>{
+    console.log(req.body);
     try{
-        user_id = req.params.user_id; 
-        item = await Categories.findOne({user_id: user_id});
-        if (!item) {
-            return res.status(404).json({ message: "Categories not found for the given user" });
-        }
-        res.send(item.categories);
-    }catch(err){
-        res.send([]);
+        const category = new Category(req.body);
+        await category.save();
+    } catch(error){
+        console.log(error)
+        res.status(500).send("Internal server error");
     }
 })
 
