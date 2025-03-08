359c7d0a99f61bda7645ab50c3998cf3247f8a27
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2025-03-08 17:53:49 -0500

    updated to use cors

--- our_app/server/forecast.py
+++ our_app/server/forecast.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, jsonify 
+from flask_cors import CORS
 import pandas as pd 
 import pmdarima as pm 
 
 
 app = Flask(__name__) 
-
+CORS(app)
+''
+print(f'app listening')
 @app.route('/forecast', methods = ['POST'])
 def forecast_transactions():
     try:
         data = request.json['transactions']
+        #print(f'data is {data}')
         df = pd.DataFrame(data)
-
+        print(f'head is {df.head(3)}')
         df['date'] = pd.to_datetime(df['date'])
         df = df.sort_values(by='date') 
         
@@ -27,13 +31,18 @@ def forecast_transactions():
 
         n_periods = 365 
         forecast_dates = pd.date_range(df_resampled.index[-1],periods = n_periods, freq = 'D')
-
+        print(f'after initializing forecast_dates')
         fitted, conf = SARIMAX_model.predict(n_periods=n_periods, return_conf_int = True)
+        print(f'after fitting to sarimax model')
 
-        forecast_df = pd.Dataframe({'date':forecast_dates, 'amount': fitted})
+        forecast_df = pd.DataFrame({'date':forecast_dates, 'amount': fitted})
+        print(f'forecast df is {forecast_df.head(3)}')
         forecast_df.set_index('date',inplace = True)
 
         forecast_json = forecast_df.reset_index().to_json(orient='records',date_format = 'iso')
+        print(f'forecast data json is {forecast_json}')
         return jsonify({"forecast": forecast_json}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+app.run(debug = False, host='localhost', port = 5000)
