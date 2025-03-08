df4f4e82c03b1bf8919dd82766b1bd0f9f7e6e78
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2025-03-08 16:49:53 -0500

    added draft of graph component and table component, looking to integrate with python backend

---
+++ our_app/frontend/public/components/ForecastTable.jsx
@@ -0,0 +1,100 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import { Accordion, AccordionSummary, AccordionDetails, Typography,
+    Card, CardContent, Box} from "@mui/material";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+
+const ForecastTable = () => {
+    const [transactions, setTransactions] = useState([]);
+    const [forecastData, setForecastData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async() => {
+            const user = await axios.get("http://localhost:8000/userInfo", {withCredentials:true});
+            const accounts = await axios.get("http://localhostL8000/user/getAccounts",{
+                params: {username: user.data.username},
+            });
+        let accountPrimaryKeys = []
+        accounts.data.forEach((account)=>{
+            accountPrimaryKeys.push(account._id);
+        });
+
+        let newTransactions = [];
+        for (let i = 0;i<accountPrimaryKeys.length;i++){
+            let key = accountPrimaryKeys[i];
+            let matchingTransactions = await axios.get("http://localhost:8000/purchases/getPurchasesByAccountId",
+                {params: {accountId:key}});
+            matchingTransactions = matchingTransactions.data;
+
+            for (let j=0;j<matchingTransactions.length;j++){
+                newTransactions.push(matchingTransactions[j]);
+            }
+        }
+        setTransactions(newTransactions);
+    };
+    
+    fetchData();
+    },[]);
+
+    const categorizeTransactions = () => {
+        const categories = {};
+        transactions.forEach((transaction) => {
+            const {type, accountId, amount, date,category} = transaction;
+            if (!categories[category]) {
+                categories[category] = [];
+            }
+
+            categories[category].push({date, amount});
+        })
+
+        return categories;
+    }
+
+    const getForecastForCategories = async() => {
+        const categories = categorizeTransactions();
+        const forecasts = [];
+        for (const category in categories){
+            const response = await axios.post("http://localhost:5000/forecast", {
+                transactions: categories[category]
+            });
+            forecasts.push({category, forecast: response.data.forecast});
+        }
+
+        setForecastData(forecasts);
+    }
+
+    useEffect(()=>{
+        if(transactions.length>0){
+            getForecastForCategories();
+        }
+    }, [transactions])
+}
+
+
+return (
+    <Box>
+        <Card>
+            <CardContent>
+                <Typography variant = "h6"> Forecasted Future Balance</Typography>
+                {forecastData.map((data,index) => (
+                    <Accordion key = {index}>
+                        <AccordionSummary
+                            expandIcon = {<ExpandMoreIcon />}
+                            aria-controls = {`panel-${data.category}-content`}
+                            id = {`panel-${data.category}-header`}>
+                                <Typography>{data.category}</Typography>
+                        </AccordionSummary>
+
+                        <AccordionDetails>
+                            <Graph
+                                data = {forecastData}
+                            />
+                        </AccordionDetails>
+                    </Accordion>
+                ))}
+            </CardContent>
+        </Card>
+    </Box>
+)
