d5543d53f2b40f3e606a9e6746ca44f589968c75
Andrew Fung <andrew@Andrews-MacBook-Pro.local>
2025-03-08 13:35:05 -0500

    update

--- our_app/frontend/public/components/Home.jsx
+++ our_app/frontend/public/components/Home.jsx
@@ -2,16 +2,18 @@ import '../../src/App.css'
 import BankAccounts from './BankAccounts';
 import BankAccountsList from './BankAccountsList';
 import Balances from './Balances';
+import Navbar from './Navbar';
 
 function Home(){
-    return (<div>
+    return (<div id = "container">
+        <Navbar/>
         <div id = "row-container">
-        <div className="column">
-            <Balances/>
+            <div className="column">
+                <Balances/>
+            </div>
+            <div className="column">two</div>
+            <div className="column"><BankAccountsList/></div>
         </div>
-        <div className="column">two</div>
-        <div className="column"><BankAccountsList/></div>
-    </div>
     </div>);
     
         
---
+++ our_app/frontend/public/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button';
+
+function Navbar(){
+    return <div id="navbar">
+        <div>LOGO</div>
+        <div>
+            <Button id = "logout-btn" variant="outlined">Logout</Button>
+        </div>
+    
+    </div>
+}
+
+export default Navbar; 
