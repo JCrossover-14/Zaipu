e31a35ac08f777641127522c4121147df8d5f0d4
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2025-03-07 22:53:07 -0500

    added purchase routes

--- our_app/server/routers/purchase.js
+++ our_app/server/routers/purchase.js
@@ -1,5 +1,52 @@
 var mongoose = require('mongoose');
 var express = require('express');
 let router = express.Router();
+let Purchase = require('../models/purchase.js');
 
 module.exports = router;
+
+
+router.post("/addPurchase", async (req, res)=>{
+    console.log("adding purchase", req.body);
+
+    type = req.body.type;
+    accountId = req.body.accountId;
+    amount = req.body.amount;
+    date = req.body.date;
+    category = req.body.category;
+
+    purchaseAttributes = {
+        type: type,
+        accountId: accountId,
+        amount: amount,
+        date: date,
+        category: category
+    }
+
+    const result = await Purchase.insertOne(purchaseAttributes);
+    res.json(result)
+})
+
+router.get("/getPurchasesByAccountId", async (req,res) =>{
+    console.log("getting purchases req body is ",req.body);
+    accountId = req.body.accountId;
+
+    try{
+        const purchases = await Purchase.find({accountId: accountId});
+        //console.log("result is ",purchases);
+        res.json(purchases);
+    } catch(error){
+        res.status(500).send("an error occured somehow");
+    }
+})
+
+router.get("/getPurchasesByCategory", async (req,res)=>{
+    console.log("getting purchases by category body is ", req.body);
+    category = req.body.category;
+    try{
+        const purchases = await Purchase.find({category: category});
+        res.json(purchases);
+    } catch(error){
+        res.status(500).send("an error occured somehow");
+    }
+})
