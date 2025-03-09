058a66c981c279063112bac70ee25bf305db294d
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2025-03-08 23:48:57 -0500

    i'm coming home

--- our_app/frontend/public/components/Home.jsx
+++ our_app/frontend/public/components/Home.jsx
@@ -5,21 +5,25 @@ import Balances from './Balances';
 import Navbar from './Navbar';
 import TransactionDistribution from './TransactionDistribution';
 import ForecastTable from './ForecastTable';
+import { Box, } from "@mui/material";
+
 function Home(){
-    return (<div id = "container">
+    return (
+    <div id = "container">
         <Navbar/>
-        <div id = "row-container">
-            <div className="column">
+        <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="flex-start" gap = {4}>
+            <Box flex={1}  minWidth="30%" maxWidth="30%" gap = {4} p = {2}>
                 <Balances/>
                 <ForecastTable/>
-            </div>
-            <div className="column">
-                <TransactionDistribution/>
+            </Box>
+            
+            <TransactionDistribution gap = {4} />
                 
-            </div>
-            <div className="column"><BankAccountsList/></div>
-
-        </div>
+            <Box flex={1} minWidth="40%" maxWidth="40%" mb = {4}>
+                <BankAccountsList/>
+            </Box>
+        </Box>
+        
     </div>);
     
         
